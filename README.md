1e91879c77ea4c6b1274e51f7a9b274e83d51836
westdom <dominic.arthur.west@gmail.com>
2025-08-19 00:01:57 +0100

    Add consecutive hit system for torrent removal

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ Couldn't find a python script to do this job so I figured why not give it a try.
 
 Details:
 
-This script checks every 10 minutes (configurable in seconds) Sonarr's, Radarr's & Lidarrs queue json information for downloads that has an `errorMessage` that states `The download is stalled with no connections` or if the download speed is less than the `DOWNLOAD_SPEED_CUTOFF` (kb/s) env var for each item in the queue and removes it, informs download client to delete files and sends the release to blocklist to prevent it from re-downloading.
+This script checks every 10 minutes (configurable in seconds) Sonarr's, Radarr's & Lidarrs queue json information for downloads that has an `errorMessage` that states `The download is stalled with no connections` or if the download speed is less than the `DOWNLOAD_SPEED_CUTOFF` (kb/s) env var for each item in the queue. 
+
+**NEW: Consecutive Hit System**: The script now uses a consecutive hit system to prevent false positives. A torrent will only be removed after it has been flagged for removal X consecutive times in a row (default: 3). This helps avoid removing torrents that are temporarily slow or stalled but recover quickly. The hit counter is stored in memory and resets if a torrent becomes healthy again or if the removal reason changes.
 
 The script uses asyncio to allow each call to wait for a response before proceeding.
 Logging defaults to the `INFO` level, but you can configure this to be e.g. `DEBUG` to get more information.
@@ -15,4 +17,11 @@ to use in a docker container, copy folder to the machine hosting your docker, `C
 
 1# `docker build -t media-cleaner .`
 
-2#. `docker run -d --name media-cleaner -e SONARR_API_KEY='123456' -e RADARR_API_KEY='123456' -e SONARR_URL='http://sonarr:8989' -e RADARR_URL='http://radarr:7878' -e API_TIMEOUT='600' -LOG_LEVEL='INFO' media-cleaner`
+2#. `docker run -d --name media-cleaner -e SONARR_API_KEY='123456' -e RADARR_API_KEY='123456' -e SONARR_URL='http://sonarr:8989' -e RADARR_URL='http://radarr:7878' -e API_TIMEOUT='600' -e LOG_LEVEL='INFO' -e CONSECUTIVE_HITS_REQUIRED='3' media-cleaner`
+
+## Environment Variables
+
+- `CONSECUTIVE_HITS_REQUIRED`: Number of consecutive times a torrent must be flagged for removal before it's actually removed (default: 3)
+- `DOWNLOAD_SPEED_CUTOFF`: Download speed threshold in kb/s below which torrents are flagged for removal
+- `API_TIMEOUT`: How often to check the queues (in seconds)
+- `LOG_LEVEL`: Logging level (INFO, DEBUG, etc.)
--- qbittorrentAPI.py
+++ qbittorrentAPI.py
@@ -11,6 +11,13 @@ QBITTORRENT_PASSWORD = os.environ["QBITTORRENT_PASSWORD"]
 
 DOWNLOAD_SPEED_CUTOFF = os.environ["DOWNLOAD_SPEED_CUTOFF"]
 
+# Global dictionary to track consecutive hits for each torrent hash
+# Format: {torrent_hash: {"hits": count, "reason": reason}}
+torrent_hit_counter = {}
+
+# Number of consecutive hits required before removal
+CONSECUTIVE_HITS_REQUIRED = int(os.environ.get("CONSECUTIVE_HITS_REQUIRED", "3"))
+
 
 async def login_to_qbittorrent(session: requests):
     await makeRequest.make_request(
@@ -52,11 +59,35 @@ def get_torrents_to_remove(torrents, category: str):
     if torrents:
         for torrent in torrents:
             if torrent["category"] == category:
-                logging.debug(f'Processing {category} queue item: {torrent["name"]}')
+                logging.debug(f'Processing {category} queue item: {torrent}')
                 SHOULD_REMOVE_TORRENT = should_remove_torrent(torrent)
+                
                 if SHOULD_REMOVE_TORRENT[0] == True:
-                    torrent["REMOVAL_REASON"] = SHOULD_REMOVE_TORRENT[1]
-                    torrents_to_remove.append(torrent)
+                    torrent_hash = torrent["hash"]
+                    reason = SHOULD_REMOVE_TORRENT[1]
+                    
+                    if torrent_hash not in torrent_hit_counter:
+                        torrent_hit_counter[torrent_hash] = {"hits": 0, "reason": reason}
+                    
+                    torrent_hit_counter[torrent_hash]["hits"] += 1
+                    torrent_hit_counter[torrent_hash]["reason"] = reason
+                    
+                    current_hits = torrent_hit_counter[torrent_hash]["hits"]
+                    logging.debug(f'{torrent["name"]} hit #{current_hits} for reason: {reason}')
+                    
+                    # Only mark for removal if we've hit the required consecutive count
+                    if current_hits >= CONSECUTIVE_HITS_REQUIRED:
+                        torrent["REMOVAL_REASON"] = SHOULD_REMOVE_TORRENT[1]
+                        torrents_to_remove.append(torrent)
+                        logging.debug(f'Marking {torrent["name"]} for removal after {current_hits} consecutive hits with reason: {reason}')
+                    else:
+                        logging.debug(f'{torrent["name"]} needs {CONSECUTIVE_HITS_REQUIRED - current_hits} more consecutive hits before removal')
+                else:
+                    torrent_hash = torrent["hash"]
+                    if torrent_hash in torrent_hit_counter:
+                        logging.debug(f'Resetting hit counter for {torrent["name"]} - torrent is now healthy')
+                        del torrent_hit_counter[torrent_hash]
+    
     return torrents_to_remove
 
 
@@ -84,9 +115,21 @@ def should_remove_torrent(torrent):
     return remove_torrent, reason
 
 
+def cleanup_hit_counter(torrents):
+    """Clean up hit counter for torrents that are no longer in the list"""
+    current_hashes = {torrent["hash"] for torrent in torrents}
+    
+    for torrent_hash in torrent_hit_counter:
+        if torrent_hash not in current_hashes:
+            logging.debug(f'Removing stale hit counter entry for hash: {torrent_hash}')
+            del torrent_hit_counter[torrent_hash]    
+
 async def remove_stalled_downloads(
     session: requests, torrents, category: str, api_url: str, api_key: str
 ):
+    # Clean up stale entries in hit counter
+    cleanup_hit_counter(torrents)
+    
     torrents_to_remove = get_torrents_to_remove(torrents, category)
     if torrents_to_remove:
         queue = await arrAPI.get_queue(api_url, api_key)
@@ -98,6 +141,12 @@ async def remove_stalled_downloads(
                         torrent["name"] in item["title"]
                         or item["title"] in torrent["name"]
                     ):
+                        # Remove from hit counter since we're actually removing the torrent
+                        torrent_hash = torrent["hash"]
+                        if torrent_hash in torrent_hit_counter:
+                            logging.debug(f'Removing hit counter entry for {torrent["name"]} after successful removal')
+                            del torrent_hit_counter[torrent_hash]
+                        
                         if category == "tv-sonarr":
                             SEASON_NUMBER = parse_season_number(item)
                             if SEASON_NUMBER:
@@ -113,7 +162,7 @@ async def remove_stalled_downloads(
                                     item["seriesId"], SEASON_NUMBER
                                 )
                                 logging.info(
-                                    f"Removing {torrent['REMOVAL_REASON']} download: {item['series']['title'] if 'series' in item else item['title']} S{SEASON_NUMBER}"
+                                    f"Removing {category}, Reason: {torrent['REMOVAL_REASON']}, Season: {SEASON_NUMBER}, Download: {item['title']}"
                                 )
                             else:
                                 await arrAPI.delete_queue_element(
@@ -124,10 +173,10 @@ async def remove_stalled_downloads(
                                     blocklist=True,
                                 )
                                 logging.info(
-                                    f"Removing {torrent['REMOVAL_REASON']} download: {item['series']['title'] if 'series' in item else item['title']}"
+                                    f"Removing {category}, Reason: {torrent['REMOVAL_REASON']}, Download: {item['title']}"
                                 )
                                 logging.warning(
-                                    f"Did not re-search sonarr download {item['series']['title'] if 'series' in item else item['title']}"
+                                    f"Did not re-search {category} download {item['title']}"
                                 )
                         elif category == "radarr" or category == "radarr-4k":
                             await arrAPI.delete_queue_element(
@@ -139,7 +188,7 @@ async def remove_stalled_downloads(
                             )
                             await delete_torrent(session, torrent)
                             logging.info(
-                                f"Removing {torrent['REMOVAL_REASON']} download: {item['title']}"
+                                f"Removing {category}, Reason: {torrent['REMOVAL_REASON']}, Download: {item['title']}"
                             )
                         else:
                             await arrAPI.delete_queue_element(
@@ -150,9 +199,13 @@ async def remove_stalled_downloads(
                                 blocklist=True,
                             )
                             logging.info(
-                                f"Removing {torrent['REMOVAL_REASON']} download: {item['movie']['title'] if 'movie' in item else item['title']}"
+                                f"Removing {category}, Reason: {torrent['REMOVAL_REASON']}, Download: {item['title']}"
                             )
                         break
+                    else:
+                        logging.warning(
+                            f"Did not find {item['title']} in {torrent['name']}"
+                        )
 
 
 def parse_season_number(item):
@@ -172,18 +225,3 @@ def parse_season_number(item):
         PARSED_SEASON_NUMBER = re.search(r"(s|S)\d\d+", item["title"])
         if PARSED_SEASON_NUMBER:
             return int(PARSED_SEASON_NUMBER.group(0)[1:])
-
-
-def parse_episode_number(item):
-    if "episode" in item and "episodeNumber" in item["episode"]:
-        return item["episode"]["episodeNumber"]
-    else:
-        PARSED_SEASON_AND_EPISODE_NUMBER = re.search(
-            r"(s|S)\d\d+(?:\S){0,2}(e|E)\d\d+", item["title"]
-        )
-        if PARSED_SEASON_AND_EPISODE_NUMBER:
-            PARSED_EPISODE_NUMBER = re.search(
-                r"(e|E)\d\d+",
-                PARSED_SEASON_AND_EPISODE_NUMBER.group(0),
-            )
-            return int(PARSED_EPISODE_NUMBER.group(0)[1:])
